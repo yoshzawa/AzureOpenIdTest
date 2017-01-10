63219a110012d5ffaa0e7af54cd778904a7b5489
t.yoshizawa <t.yoshizawa@jc-21.jp>
2017-01-10 13:41:54 +0900

    add error handring

--- src/com/gmail/yoshzawa/h28jk3aopenidtest/MicrosoftRedirectServlet.java
+++ src/com/gmail/yoshzawa/h28jk3aopenidtest/MicrosoftRedirectServlet.java
@@ -14,9 +14,13 @@ public class MicrosoftRedirectServlet extends HttpServlet implements AzureConsta
 		
 		String id_token = req.getParameter("id_token");
 		String state = req.getParameter("state");
+		String error = req.getParameter("error");
+		String error_description = req.getParameter("error_description");
 		
 		resp.getWriter().println("id_token = " + id_token);
 		resp.getWriter().println("state = " + state);
+		resp.getWriter().println("error = " + error);
+		resp.getWriter().println("error_description = " + error_description);
 		
 		
 		
