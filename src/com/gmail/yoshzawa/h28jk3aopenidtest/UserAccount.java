85cf7ab841b33cb2bbd38e1a4d7d25a93ee9aa3a
t.yoshizawa <t.yoshizawa@jc-21.jp>
2017-01-18 15:11:07 +0900

    create Objectify UserAccount

--- src/com/gmail/yoshzawa/h28jk3aopenidtest/UserAccount.java
+++ src/com/gmail/yoshzawa/h28jk3aopenidtest/UserAccount.java
@@ -1,6 +1,38 @@
 package com.gmail.yoshzawa.h28jk3aopenidtest;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Parent;
+
 public class UserAccount {
+	@Id
+	private Long id;
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public UserAccount(String email){
+		setEmail(email);
+	}
+	
+	public void ofyInit(){
+		ObjectifyService.register(UserAccount.class);
+	}
+	
 	
 
 }
