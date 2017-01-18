107c0129730d5300d72c6787433d090e25a5dd21
yoshzawa <yoshzawa@users.noreply.github.com>
2017-01-18 14:53:54 +0900

    some changes (#1)
    * some change
    
    * some changes
    
    * some change

--- src/com/gmail/yoshzawa/h28jk3aopenidtest/AzureConstant.java
+++ src/com/gmail/yoshzawa/h28jk3aopenidtest/AzureConstant.java
@@ -1,5 +1,6 @@
 package com.gmail.yoshzawa.h28jk3aopenidtest;
 
 public interface AzureConstant {
+	public static final String AzureAppName="h28jk3aopenidtest.appspot.com";
 	public static final String AzureAppId="3644e4dd-b2d8-40f1-9c96-e2dfcf88d070";
 }
